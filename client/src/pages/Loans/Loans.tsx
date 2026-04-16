62b540dd9a364d69db7f01c6057454b389b00938
sabres207 <sagesdoron@gmail.com>
2026-04-16 18:02:01 +0300

    basic loans page

--- client/src/pages/Loans/Loans.tsx
+++ client/src/pages/Loans/Loans.tsx
@@ -1,8 +1,9 @@
-import type { FC } from 'react'
+import { useState, type FC } from 'react'
 import { CreateLoanModal } from './CreateLoanModal'
 import { graphql } from '../../__generated__'
 import { useQuery } from '@apollo/client'
 import { QueryResult } from '../../components/QueryResult'
+import styled from 'styled-components'
 
 const GET_LOANS = graphql(`
   query Loans {
@@ -15,16 +16,20 @@ const GET_LOANS = graphql(`
 
 export const Loans: FC<{}> = () => {
   const { loading, error, data } = useQuery(GET_LOANS)
+  const [showModal, setShowModal] = useState(false)
 
   return (
     <div>
-      <h1>Loans Page</h1>
-      <CreateLoanModal />
+      <Title>Loans Page</Title>
       <QueryResult loading={loading} error={error}>
         {data?.loans.map((loan) => (
           <div key={loan.id}>{loan.name}</div>
         ))}
       </QueryResult>
+      <button onClick={() => setShowModal(true)}>Create Loan</button>
+      <CreateLoanModal showModal={showModal} setShowModal={setShowModal} />
     </div>
   )
 }
+
+const Title = styled.h1``
