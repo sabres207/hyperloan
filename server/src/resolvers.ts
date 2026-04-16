b334c02c2abee700539a07fe2128b6dc5a8db11f
sabres207 <sagesdoron@gmail.com>
2026-04-16 14:31:28 +0300

    add schema

--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -1,4 +1,7 @@
+import { DateResolver } from 'graphql-scalars'
+
 export const resolvers = {
+  Date: DateResolver,
   Query: {},
   Mutation: {},
   Loan: {},
--- server/src/schema.graphql.ts
+++ server/src/schema.graphql.ts
@@ -1,3 +1,46 @@
 import { gql } from 'graphql-tag'
 
-export const typeDefs = gql``
+export const typeDefs = gql`
+  scalar Date
+
+  type Query {
+    loans: [Loan!]! # TODO: add pagination
+    loan(id: ID!): Loan
+  }
+
+  type Mutation {
+    createLoan(createLoanInput: CreateLoanInput!): Loan
+  }
+
+  type Loan {
+    id: ID!
+    name: String!
+    principalAmount: Float!
+    startDate: Date!
+    endDate: Date!
+    totalExpectedInterest: Float!
+    repaymentSchedule: [Repayment!]!
+  }
+
+  type Repayment {
+    id: ID!
+    paymentDate: Date!
+    paymentType: PaymentType!
+    principalComponent: Float!
+    interestComponent: Float!
+    totalPayment: Float!
+    remainingBalance: Float!
+  }
+
+  enum PaymentType {
+    INTEREST
+    PRINCIPAL_PLUS_INTEREST
+  }
+
+  type CreateLoanInput {
+    name: String!
+    principalAmount: Float!
+    startDate: Date!
+    endDate: Date!
+  }
+`
