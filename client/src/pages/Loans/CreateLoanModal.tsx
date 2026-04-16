7e1c567c4b49fc57c48b633e1ca73dcc0628f6b2
sabres207 <sagesdoron@gmail.com>
2026-04-16 17:56:25 +0300

    new Create Loan Modal component using the Modal

--- client/src/pages/Loans/CreateLoanModal.tsx
+++ client/src/pages/Loans/CreateLoanModal.tsx
@@ -1,3 +1,22 @@
-export const CreateLoanModal = () => {
-  return <div>Create Loan Modal</div>
+import { type Dispatch, type FC, type SetStateAction } from 'react'
+import { Modal } from '../../components/Modal'
+
+type CreateLoanModalProps = {
+  showModal: boolean
+  setShowModal: Dispatch<SetStateAction<boolean>>
+}
+
+export const CreateLoanModal: FC<CreateLoanModalProps> = ({
+  showModal,
+  setShowModal,
+}) => {
+  return (
+    <Modal
+      isOpen={showModal}
+      onClose={() => setShowModal(false)}
+      title="Create new loan"
+    >
+      <div>TODO: create loan form</div>
+    </Modal>
+  )
 }
