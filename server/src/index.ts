303fd2e13120a047293083090ee68e63156dfaed
sabres207 <sagesdoron@gmail.com>
2026-04-20 15:49:09 +0300

    import

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,7 @@ import { ApolloServer } from '@apollo/server'
 import { startStandaloneServer } from '@apollo/server/standalone'
 import { typeDefs } from './schema.graphql.js'
 import { resolvers } from './resolvers.js'
-import { AppDataSource } from './data-source.js'
+import { AppDataSource } from './db/data-source.js'
 import { Context, createContext } from './context.js'
 
 async function main() {
